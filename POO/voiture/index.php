cd4e10b457a8872f90af6dd13b18635a85a52067
KCharih <kcharih@arfp.asso.fr>
2021-03-10 16:23:36 +0100

    Add new features

--- POO/voiture/index.php
+++ POO/voiture/index.php
@@ -14,7 +14,7 @@ require_once 'Voiture.php';
 $v2 = new Voiture("Renault", "Clio");
 //echo 'La voiture V2 est de marque ' . $v2->get_marque() . ' et de modèle ' . $v2->get_modele() . '<br>';
 
-$v2->afficherVoiture();
+/* $v2->afficherVoiture();
 $v2->accelerer(20, "ville");
 $v2->afficherVoiture();
 $v2->freiner();
@@ -27,14 +27,14 @@ $v2->accelerer(100, "circuit");
 $nb = 5;
 echo 'ma variable nb vaut : ' . $nb . ' <br>';
 $v2->ralentir($nb);
-echo 'ma variable nb vaut : ' . $nb . ' <br>';
+echo 'ma variable nb vaut : ' . $nb . ' <br>'; */
 
 $v1 = new Voiture("Peugeot", "3008");
-$v1->accelerer(50, "ville");
+/* $v1->accelerer(50, "ville");
 $v1->afficherVoiture();
 $v2->depasser($v1);
-$v1->afficherVoiture();
+$v1->afficherVoiture(); */
 
 $v3 = new Voiture("BMW", "Serie 3");
 
-echo 'Nombre de voitures crées : '. $v1::$nb_voiture_crees . '<br>';
+echo 'Nombre de voitures crées : '. $v2::$nb_voiture_crees . '<br>';
